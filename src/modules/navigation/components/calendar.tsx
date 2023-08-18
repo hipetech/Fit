a7c0cff3dc91a75fc4c208d3dcbb3e679e9e8bb7
hipetech <korota1kirill@gmail.com>
2023-08-18 22:54:17 +0300

    changed colors

--- src/modules/navigation/components/calendar.tsx
+++ src/modules/navigation/components/calendar.tsx
@@ -7,19 +7,19 @@ import SmallIconButton from "../../../components/smallIconButton";
 
 const Calendar = () => {
 
-    return (
-        <View style={styles.container}>
-            <SmallIconButton Content={CalenderIcon} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <SmallIconButton Content={CalenderIcon} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        paddingRight: 25,
-        flexDirection: "row",
-        gap: 18
-    }
+  container: {
+    paddingRight: 25,
+    flexDirection: "row",
+    gap: 18
+  }
 });
 
 export default Calendar;
